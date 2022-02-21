05557dbc51b5c100b315335fec55b0aa66a90f59
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-02-21 16:01:36 -0500

    adding urls_new and some functions

--- express_server.js
+++ express_server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -22,22 +24,17 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
+// app.get("/set", (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+// });
  
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-});
-
-// app.get("/urls", (req, res) => {
-//   const templateVars = { urls: urlDatabase };
-//   res.render("urls_index", templateVars);
+// app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);
 // });
 
 app.get("/urls", (req, res) => {
@@ -45,7 +42,29 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] /* What goes here? */ };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+const  generateRandomString = function() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
+  let shortURL = [];
+  for (let i = 0; i <= 6; i++) {
+    shortURL.push(characters[Math.floor(Math.random() * 62)]);
+  }
+  shortURL = shortURL.join('');
+  console.log(shortURL);
+  return shortURL;
+};
+
+generateRandomString();
